7bb906c65db944bb11b1a3970e2bbdfdf01fdae6
Howard <howardjer@gmail.com>
2021-02-10 22:50:54 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import dbcreds
+import mariadb
+import sys
+
+conn = mariadb.connect(
+    user = dbcreds.user,
+    password = dbcreds.password,
+    host = dbcreds.host,
+    port = dbcreds.port,
+    database = dbcreds.database    
+)
+cursor = conn.cursor()
+cursor.execute(
+    "INSERT INTO assignment_db (username, content) VALUES (?,?)",
+    ['Howard', 'HoHo']
+)
+
+conn.commit()
+cursor.close()
+conn.close()
